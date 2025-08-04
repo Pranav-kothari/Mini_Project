af0e178884a880da913c7ea2ca96b1e4fbb700c3
Pranav-kothari <pranav.kothari2022@vitstudent.ac.in>
2025-08-04 14:42:13 +0530

    Update settings.py

--- E_commerce/settings.py
+++ E_commerce/settings.py
@@ -143,6 +143,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'myshopee762@gmail.com'
-EMAIL_HOST_PASSWORD = 'ojes egtl gjiz tmpu'
+EMAIL_HOST_USER = 'myshopee@gmail.com'
+EMAIL_HOST_PASSWORD = 'Your_key'
 
